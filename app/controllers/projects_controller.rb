6ea6e0807d4c275e26d6f4b519035bc2815f56bf
Makoto Ejima <makotoejima@gmail.com>
2016-07-08 18:25:43 -0700

    project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,16 @@
 class ProjectsController < ApplicationController
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users do
     resources :profile
+
   end
 end
