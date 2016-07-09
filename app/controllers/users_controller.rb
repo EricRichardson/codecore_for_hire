bda36d2f2a1e955e59b21949b14d3cdc26640d8a
Wilson Lu <huiquanlu@gmail.com>
2016-07-08 18:26:04 -0700

    user authentication/session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:new, :create, :reset]
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Logged In!"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :for_hire, :profile_picture)
+  end
+
+  def find_user
+    @user = User.find(session[:user_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resources :users do
     resources :profile
   end
+  resources :sessions, only: [:create, :new, :destroy] do
+    delete :destroy, on: :collection
+  end
 end
