df9dbee938d6bdf56ce165970e31694ab0496688
Eric Richardson <eric@ericonrails.com>
2016-07-08 22:14:17 -0700

    Fixed typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
     resources :profile, only: [:new, :create, :update, :destroy] do
       resources :experience, only: [:create, :edit, :update, :destroy]
-
+    end
   end
 
   resources :sessions, only: [:create, :new, :destroy] do
