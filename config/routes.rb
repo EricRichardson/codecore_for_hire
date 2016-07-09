ed25a992a94a89f49bf24519e48515f545525941
Matthew Asaminew <mattasaminew@gmail.com>
2016-07-09 14:03:14 -0700

    Added search function

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :search, param: :search
+
   resources :sessions, only: [:create, :new, :destroy] do
     delete :destroy, on: :collection
   end
