44916a30e92f0e4ce5975b4810b8e6d0817aeef5
Makoto Ejima <makotoejima@gmail.com>
2016-07-09 09:32:52 -0700

    Added basic navbar and notice and flash alerts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,10 +25,14 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find params[:id]
   end
 
   def update
-
+    @user = User.find params[:id]
+    if @user.update user_params
+      redirect_to root_path
+    end
   end
 
 
