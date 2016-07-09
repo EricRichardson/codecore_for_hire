79fa035fddb235638e5dab2c0926c7f95d112224
Ryan Woo <rjwoo@sfu.ca>
2016-07-08 18:23:53 -0700

    added views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users do
-    resources :profile
+    resources :profile, only: [:new, :create, :update, :destroy] do
+      resources :experience, only: [:create, :edit, :update, :destroy] 
   end
 
 end
