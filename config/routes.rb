e90831a0aa50409b1e271393f5322c229db898b0
Ryan Woo <rjwoo@sfu.ca>
2016-07-08 20:15:31 -0700

    finished crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   resources :users do
-
-    resources :profile, only: [:new, :create, :update, :destroy] do
-      resources :experience, only: [:create, :edit, :update, :destroy]
+    resources :profiles, only: [:new, :create, :update, :destroy] do
+      resources :experiences , only: [:create, :edit, :update, :destroy]
+    end
+  end
 
   end
 
   resources :sessions, only: [:create, :new, :destroy] do
     delete :destroy, on: :collection
 
-  end
+
 
 end
