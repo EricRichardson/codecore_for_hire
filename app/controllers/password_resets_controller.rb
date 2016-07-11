beeb67db64a723eb343c5bb95719694485caf38f
Eric Richardson <eric@ericonrails.com>
2016-07-10 21:52:14 -0700

    Put mailer on background job

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -9,7 +9,7 @@ class PasswordResetsController < ApplicationController
       token = SecureRandom.urlsafe_base64
       user.reset_token = token
       url = edit_password_reset_url(user) + "/?token=#{token}"
-      PasswordResetsMailer.send_reset(user, url).deliver_now
+      PasswordResetsMailer.send_reset(user, url).deliver_later
       redirect_to root_path, notice: "Reset email sent"
     else
       redirect_to root_path
